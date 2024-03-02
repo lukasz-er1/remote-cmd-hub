366c72605d80263d8cda9433cd6165621a39c605
Lukasz R <33703141+lukasz-er1@users.noreply.github.com>
2024-03-02 23:01:45 +0100

    Add sending cmd output from client and save it to json file on the server

--- app/app.py
+++ app/app.py
@@ -1,11 +1,45 @@
-from flask import Flask
+import json
+from flask import Flask, request
 
 app = Flask(__name__)
 
 
-@app.route("/")
-def main():
-    return "ls /usr/"
+@app.get("/cmd1")
+def usr():
+    return "ls /usr"
+
+
+@app.get("/cmd2")
+def opt():
+    return "ls /opt"
+
+
+@app.post("/output")
+def output_post():
+    try:
+        with open(f"output_history.json", "r") as json_file:
+            output_history: dict = json.load(json_file)
+    except (json.JSONDecodeError, FileNotFoundError):
+        output_history: dict = {"history": []}
+
+    data: dict = request.get_json()
+    output_history["history"].append(data)
+
+    with open(f"output_history.json", "w+") as json_file:
+        json.dump(obj=output_history, fp=json_file, indent=4)
+
+    return "OK", 201
+
+
+@app.get("/output")
+def output_get():
+    try:
+        with open(f"output_history.json", "r") as json_file:
+            output_history = json.load(json_file)
+    except (json.JSONDecodeError, FileNotFoundError):
+        output_history: dict = {"history": []}
+
+    return output_history
 
 
 if __name__ == "__main__":
--- app/client.py
+++ app/client.py
@@ -1,12 +1,21 @@
 import requests
 import subprocess
+from time import sleep
+from datetime import datetime
+
 
 api_url = "http://127.0.0.1:5000"
 
 
-if __name__ == "__main__":
-    cmd = requests.get(api_url)
+def execute_cmd_from_subpage(subpage: str) -> None:
+    cmd = requests.get(f"{api_url}/{subpage}")
+    curr_time = datetime.now().strftime("%H:%M:%S")
     output = subprocess.getoutput(cmd.text)
+    data: dict = {"time": curr_time, "cmd": cmd.text, "output": output}
+    requests.post(f"{api_url}/output", json=data)
 
-    data = {"cmd": cmd.text, "output": output}
-    print(data)
+
+if __name__ == "__main__":
+    execute_cmd_from_subpage("cmd1")
+    sleep(2)
+    execute_cmd_from_subpage("cmd2")

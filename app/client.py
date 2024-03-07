674e30b0bd1b8b343d036a5a01dd7c9ea6db2b6a
Lukasz R <33703141+lukasz-er1@users.noreply.github.com>
2024-03-07 23:33:54 +0100

    Add creating unique ID for client.py

--- app/client.py
+++ app/client.py
@@ -1,5 +1,7 @@
+import uuid
 import requests
 import subprocess
+from dotenv import dotenv_values, set_key
 from time import sleep
 from datetime import datetime
 
@@ -16,6 +18,12 @@ def execute_cmd_from_subpage(subpage: str) -> None:
 
 
 if __name__ == "__main__":
-    execute_cmd_from_subpage("cmd1")
-    sleep(2)
-    execute_cmd_from_subpage("cmd2")
+    config = dotenv_values(".env")
+    machinde_id = config.get("machinde_id")
+    if machinde_id is None:
+        new_id = str(uuid.uuid4())
+        set_key(".env", "machinde_id", new_id)
+
+    # execute_cmd_from_subpage("cmd1")
+    # sleep(2)
+    # execute_cmd_from_subpage("cmd2")

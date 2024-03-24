182ba7bb960a900dfcb4995dd70b8a848ba2c768
Lukasz R <33703141+lukasz-er1@users.noreply.github.com>
2024-03-24 21:04:44 +0100

    Add flask_login

--- app/app.py
+++ app/app.py
@@ -5,10 +5,43 @@ from app_functions import (
     get_current_timestamp,
     last_ping_time_ago,
 )
-from flask import Flask, request, render_template, redirect
+from flask import Flask, request, render_template, redirect, url_for
+import flask_login
 
 app = Flask(__name__)
+app.secret_key = "Super_secret_KEY!$%"
 app.url_map.strict_slashes = False
+
+login_manager = flask_login.LoginManager()
+login_manager.init_app(app)
+users = {"Lukasz": {"password": "xxx"}}
+
+
+class User(flask_login.UserMixin):
+    pass
+
+
+@login_manager.user_loader
+def user_loader(email):
+    if email not in users:
+        return
+
+    user = User()
+    user.id = email
+
+    return user
+
+
+@login_manager.request_loader
+def request_loader(request):
+    email = request.form.get("email")
+    if email not in users:
+        return
+
+    user = User()
+    user.id = email
+
+    return user
 
 
 @app.get("/")
@@ -23,9 +56,36 @@ def home():
     return render_template("home.html", machines=machines)
 
 
-@app.get("/login")
+@app.route("/login", methods=["GET", "POST"])
 def login():
-    return render_template("login.html")
+    if request.method == "GET":
+        return render_template("login.html")
+
+    email = request.form["email"]
+    if email in users and request.form["password"] == users[email]["password"]:
+        user = User()
+        user.id = email
+        flask_login.login_user(user)
+        return redirect(url_for("protected"))
+
+    return "Bad login"
+
+
+@app.route("/protected")
+@flask_login.login_required
+def protected():
+    return "Logged in as: " + flask_login.current_user.id
+
+
+@app.route("/logout")
+def logout():
+    flask_login.logout_user()
+    return "Logged out"
+
+
+@login_manager.unauthorized_handler
+def unauthorized_handler():
+    return "Unauthorized", 401
 
 
 @app.get("/<machine_id>")
